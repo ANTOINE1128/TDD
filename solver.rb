2fe30109b78f92475a259b045f32f7dd157195d6
ANTOINE1128 <makdessyantoine@gmail.com>
2023-08-11 19:27:53 +0300

    write method for solver.rb file

--- solver.rb
+++ solver.rb
@@ -1,12 +1,24 @@
 class Solver
   def factorial(n)
-    return 1 if n == 0
+    return 1 if n.zero?
 
-    raise ArgumentError, "Argument must be a non-negative integer" if n < 0
+    raise ArgumentError, 'Argument must be a non-negative integer' if n.negative?
 
     result = 1
     (1..n).each { |i| result *= i }
     result
+  end
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(number)
+    return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
+    return 'fizz' if (number % 3).zero?
+    return 'buzz' if (number % 5).zero?
+
+    number.to_s
   end
 end
 
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,57 +1,56 @@
 require_relative '../solver'
 
 describe Solver do
-
   before :each do
     @fact = Solver.new
   end
 
-  context "Given 3" do
-    it "Should return 6" do
+  context 'Given 3' do
+    it 'Should return 6' do
       expect(@fact.factorial(3)).to be 6
     end
   end
 
-  context "Given 0" do
-    it "Should return 1" do
+  context 'Given 0' do
+    it 'Should return 1' do
       expect(@fact.factorial(0)).to be 1
     end
   end
 
-  context "Given -3" do
-    it "Should raise ArgumentError" do
-      expect(@fact.factorial(-3)).raise_error("ArgumentError")
+  context 'Given -3' do
+    it 'Should raise ArgumentError' do
+      expect { @fact.factorial(-3) }.to raise_error(ArgumentError)
     end
   end
 
-  describe "#reverse_string" do
+  describe '#reverse' do
     context "Given 'hello'" do
       it "Should return 'olleh'" do
-        expect(@fact.reverse_string('hello')).to eq 'olleh'
+        expect(@fact.reverse('hello')).to eq 'olleh'
       end
     end
   end
 
-  describe "#fizzbuzz" do
-    context "Given 3" do
+  describe '#fizzbuzz' do
+    context 'Given 3' do
       it "Should return 'fizz'" do
         expect(@fact.fizzbuzz(3)).to eq 'fizz'
       end
     end
 
-    context "Given 5" do
+    context 'Given 5' do
       it "Should return 'buzz'" do
         expect(@fact.fizzbuzz(5)).to eq 'buzz'
       end
     end
 
-    context "Given 15" do
+    context 'Given 15' do
       it "Should return 'fizzbuzz'" do
         expect(@fact.fizzbuzz(15)).to eq 'fizzbuzz'
       end
     end
 
-    context "Given 7" do
+    context 'Given 7' do
       it "Should return '7'" do
         expect(@fact.fizzbuzz(7)).to eq '7'
       end
