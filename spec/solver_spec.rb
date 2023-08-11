5baa5994c5a02d3de4e81396b25eac2ebb6e785d
forLoop94 <charlesuwem94@gmail.com>
2023-08-11 17:01:15 +0100

    Add tests for reverse_string and fizzbuzz methods

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -24,4 +24,37 @@ describe Solver do
     end
   end
 
+  describe "#reverse_string" do
+    context "Given 'hello'" do
+      it "Should return 'olleh'" do
+        expect(@fact.reverse_string('hello')).to eq 'olleh'
+      end
+    end
+  end
+
+  describe "#fizzbuzz" do
+    context "Given 3" do
+      it "Should return 'fizz'" do
+        expect(@fact.fizzbuzz(3)).to eq 'fizz'
+      end
+    end
+
+    context "Given 5" do
+      it "Should return 'buzz'" do
+        expect(@fact.fizzbuzz(5)).to eq 'buzz'
+      end
+    end
+
+    context "Given 15" do
+      it "Should return 'fizzbuzz'" do
+        expect(@fact.fizzbuzz(15)).to eq 'fizzbuzz'
+      end
+    end
+
+    context "Given 7" do
+      it "Should return '7'" do
+        expect(@fact.fizzbuzz(7)).to eq '7'
+      end
+    end
+  end
 end
